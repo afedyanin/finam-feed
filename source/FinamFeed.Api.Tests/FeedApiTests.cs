156192dea14ec8842bb2564892393e0f92ea9dde
Anatoly <anfemail@gmail.com>
2018-07-18 11:26:45 +0300

    Update packages

--- source/FinamFeed.Api.Tests/FeedApiTests.cs
+++ source/FinamFeed.Api.Tests/FeedApiTests.cs
@@ -6,7 +6,7 @@
     using FinamFeed.Api.Config;
     using NUnit.Framework;
 
-    [TestFixture, Explicit]
+    [TestFixture]
     public class FeedApiTests
     {
         private IApiConfiguration config;
